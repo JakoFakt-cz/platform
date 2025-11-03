8a5df6c1b126eca71d7a365d76afb30efeab9118
Klimondra <klimondra@seznam.cz>
2025-11-03 09:07:28 +0100

    fix: Added comment
    Took 1 minute

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('ENTER_YOUR_MONGODB_CONNECTION_STRING_HERE'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
