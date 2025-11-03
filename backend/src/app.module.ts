97181945e8c0a6fe14549bb32935b4a21d1d6f36
Klimondra <klimondra@seznam.cz>
2025-11-03 09:50:07 +0100

    feature: Added .env and Connected database via MongooseModule
    Took 41 minutes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('ENTER_YOUR_MONGODB_CONNECTION_STRING_HERE'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING ?? ''),
   ],
   controllers: [AppController],
   providers: [AppService],
