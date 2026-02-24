77eebda8c3929d4fad74ce82cac8fc9029076ec2
Klimondra <klimondra@seznam.cz>
2026-02-24 11:14:37 +0100

    fix: removed sensitive data from client
    Took 4 minutes

--- frontend/actions/user.ts
+++ frontend/actions/user.ts
@@ -1,31 +1,18 @@
-"use server";
+'use server';
 
 export interface UserModel {
   username: string;
-  displayName: string,
-  email: string;
+  displayName: string;
   profilePictureUrl: string;
 }
 
-export interface UnsecureUserModel extends UserModel {
-  emailVerified: boolean;
-  passwordHash: string;
-}
-
-export async function RetrieveUserByIdFromBackend({
-  id,
-}: {
-  id: string,
-}) {
-  const res = await fetch(
-    `${process.env.BACKEND_URL}/articles/exact?id=${id}`,
-    {
-      method: "GET",
-    }
-  );
+export async function RetrieveUserByIdFromBackend({ id }: { id: string }) {
+  const res = await fetch(`${process.env.BACKEND_URL}/users/id?id=${id}`, {
+    method: 'GET',
+  });
 
   if (!res.ok) {
-    throw new Error(`Failed to find article with id ${id}. ${res.statusText}`);
+    throw new Error(`Failed to find user with id ${id}. ${res.statusText}`);
   }
 
   return res.json();
